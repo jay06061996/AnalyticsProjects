c816f8d4871e407a3205ca40e2ca652ff355ff79
Jaydeep Solanki <112417566+jay06061996@users.noreply.github.com>
2023-04-24 19:46:34 +0530

    Update Housing Data.sql

--- Housing Data.sql
+++ Housing Data.sql
@@ -68,35 +68,8 @@ Add PropertySplitCity Nvarchar(255);
 Update NashvilleHousing
 SET PropertySplitCity = SUBSTRING(PropertyAddress, CHARINDEX(',', PropertyAddress) + 1 , LEN(PropertyAddress))
 
-
-Select OwnerAddress
+Select PropertySplitAddress, PropertySplitCity
 From NashvilleHousing
-
-
-Select
-PARSENAME(REPLACE(OwnerAddress, ',', '.') , 3)
-,PARSENAME(REPLACE(OwnerAddress, ',', '.') , 2)
-,PARSENAME(REPLACE(OwnerAddress, ',', '.') , 1)
-From NashvilleHousing
-
-
-ALTER TABLE NashvilleHousing
-Add OwnerSplitAddress Nvarchar(255);
-
-Update NashvilleHousing
-SET OwnerSplitAddress = PARSENAME(REPLACE(OwnerAddress, ',', '.') , 3)
-
-ALTER TABLE NashvilleHousing
-Add OwnerSplitCity Nvarchar(255);
-
-Update NashvilleHousing
-SET OwnerSplitCity = PARSENAME(REPLACE(OwnerAddress, ',', '.') , 2)
-
-ALTER TABLE NashvilleHousing
-Add OwnerSplitState Nvarchar(255);
-
-Update NashvilleHousing
-SET OwnerSplitState = PARSENAME(REPLACE(OwnerAddress, ',', '.') , 1)
 
 
 -- Change Y and N to Yes and No in "Sold as Vacant" field
@@ -107,16 +80,17 @@ Group by SoldAsVacant
 order by 2
 
 
-Select SoldAsVacant
-, CASE When SoldAsVacant = 'Y' THEN 'Yes'
-	   When SoldAsVacant = 'N' THEN 'No'
-	   ELSE SoldAsVacant
-	   END
+Select SoldAsVacant,
+CASE When SoldAsVacant = 'Y' THEN 'Yes'
+     When SoldAsVacant = 'N' THEN 'No'
+     ELSE SoldAsVacant
+     END
 From NashvilleHousing
 
 
 Update NashvilleHousing
-SET SoldAsVacant = CASE When SoldAsVacant = 'Y' THEN 'Yes'
+SET SoldAsVacant = CASE 
+           When SoldAsVacant = 'Y' THEN 'Yes'
 	   When SoldAsVacant = 'N' THEN 'No'
 	   ELSE SoldAsVacant
 	   END
@@ -126,32 +100,26 @@ SET SoldAsVacant = CASE When SoldAsVacant = 'Y' THEN 'Yes'
 
 WITH RowNumCTE AS
 (
-Select *,
-	ROW_NUMBER() OVER (
-	PARTITION BY ParcelID,
+Select *, ROW_NUMBER() OVER (PARTITION BY ParcelID,
 				 PropertyAddress,
 				 SalePrice,
 				 SaleDate,
 				 LegalReference
-				 ORDER BY
-					UniqueID
-					) row_num
+				 ORDER BY UniqueID) row_num
 
 From NashvilleHousing
 --order by ParcelID
 )
 
-Select *
+Delete
 From RowNumCTE
 Where row_num > 1
-Order by PropertyAddress
 
 
 -- Delete Unused Columns
 
-Select *
-From NashvilleHousing
-
 ALTER TABLE NashvilleHousing
 DROP COLUMN OwnerAddress, TaxDistrict, PropertyAddress, SaleDate
 
+Select *
+From NashvilleHousing
